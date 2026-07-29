03c31ba88043ccf97602cd8200941e9cf2918b9c
Damian Wujec <wujecdamian12@gmail.com>
2026-07-29 03:42:34 +0200

    refactor: move passport initialization and session to main App.ts file
    fixed: scope issue (would work only on auth routes)

--- Backend/src/App.ts
+++ Backend/src/App.ts
@@ -1,7 +1,33 @@
 import express, { type Express, type Request, type Response } from "express";
 import userRouter from "./Routers/UserRouter";
 import authRouter from "./Routers/AuthRouter";
+import { createClient } from "redis";
+import { RedisStore } from "connect-redis";
+import session from "express-session";
+import passport from "passport";
 const app: Express = express();
+
+// 1. Initialize Redis and Session
+let redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+});
+
+app.use(
+  session({
+    store: redisStore,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.REDIS_SECRET as string,
+  }),
+);
+
+// 2. Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
--- Backend/src/Routers/AuthRouter.ts
+++ Backend/src/Routers/AuthRouter.ts
@@ -1,34 +1,9 @@
-import { Router, type Request, type Response } from "express";
-import passport from "passport";
-import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client/extension";
-import cookieSession from "cookie-session";
-import session from "express-session";
-import { RedisStore } from "connect-redis";
-import { createClient } from "redis";
+import { Router } from "express";
 import * as authController from "../Controllers/AuthController";
-import dotenv from "dotenv";
-dotenv.config();
 
 const router = Router();
-let redisClient = createClient();
-redisClient.connect().catch(console.error);
 
-let redisStore = new RedisStore({
-  client: redisClient,
-  prefix: "myapp:",
-});
-
-router.use(
-  session({
-    store: redisStore,
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.REDIS_SECRET as string, //as string because env can be undefined
-  }),
-);
-router.use(passport.initialize());
-router.use(passport.session());
+// Routes
 router.get("/", authController.greetUser);
 router.get("/error", authController.loginError);
 router.get("/github", authController.authenticateUser);

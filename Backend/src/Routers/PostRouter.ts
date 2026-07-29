af219028838abab37dfd786083de2a4906c8f2fb
Damian Wujec <wujecdamian12@gmail.com>
2026-07-29 20:54:00 +0200

    fix: remove unused home page route from PostRouter

--- Backend/src/Routers/PostRouter.ts
+++ Backend/src/Routers/PostRouter.ts
@@ -7,13 +7,6 @@ import * as PostController from "../Controllers/PostController";
 
 const router = express.Router();
 
-// middleware that is specific to this router
-
-// define the home page route
-router.get("/", (req: Request, res: Response) => {
-  console.log(req.user);
-  res.json({ user: req.user });
-});
 //get user / group posts
 router.get("/", PostController.getAllPosts);
 router.get("/:userId", PostController.getFollowingPosts);

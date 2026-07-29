43ec61b00fcd94202eb9df28864194bd2921e095
Damian Wujec <wujecdamian12@gmail.com>
2026-07-29 18:29:50 +0200

    feat: create User Router and Controller with its CRUD

--- Backend/src/Routers/UserRouter.ts
+++ Backend/src/Routers/UserRouter.ts
@@ -1,13 +1,28 @@
-import express, { type Request, type Response, type NextFunction } from 'express';
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
+import * as UserController from "../Controllers/UserController";
 
 const router = express.Router();
 
 // middleware that is specific to this router
 
 // define the home page route
-router.get('/', (req: Request, res: Response) => {
-  
+router.get("/", (req: Request, res: Response) => {
+  console.log(req.user);
+  res.json({ user: req.user });
 });
+//user delete
+router.delete("/delete", UserController.deleteUser);
 
+//user edit routes
+router.put("/edit/bio", UserController.editUserBio);
+router.put("/edit/tags", UserController.editUserTags);
+router.put("/edit/socials", UserController.editUserSocials);
+router.put("/edit/displayName", UserController.editUserDisplayName);
+router.put("/edit/profilePicture", UserController.editUserPfp);
+router.put("/edit/bannerPicture", UserController.editUserBanner);
 
 export default router;

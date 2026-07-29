8cd3e62ab783d2eb4d668c74ddbcecce107db386
Damian Wujec <wujecdamian12@gmail.com>
2026-07-29 02:08:35 +0200

    feat: update Prisma configuration and migration for GitHub user module

---
+++ prisma/migrations/20260728232558_github_user_module/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[github_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `github_id` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "User_username_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "password",
+DROP COLUMN "username",
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "github_id" TEXT NOT NULL,
+ALTER COLUMN "profile_picture_url" DROP NOT NULL,
+ALTER COLUMN "profile_picture_url" DROP DEFAULT,
+ALTER COLUMN "banner_picture_url" DROP NOT NULL,
+ALTER COLUMN "banner_picture_url" DROP DEFAULT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");

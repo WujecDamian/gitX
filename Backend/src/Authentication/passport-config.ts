63d17fdad8000d649033bd204f65f6f8ead4acfe
Damian Wujec <wujecdamian12@gmail.com>
2026-07-29 02:08:59 +0200

    feat: update passport serialize, deserialize User functions to store User info in db

--- Backend/src/Authentication/passport-config.ts
+++ Backend/src/Authentication/passport-config.ts
@@ -1,14 +1,31 @@
 import passport from "passport";
 import { Strategy as GitHubStrategy, type Profile } from "passport-github2";
+import { prisma } from "../lib/prisma";
+import { type User as prismaUser } from "../generated/prisma/client";
 import dotenv from "dotenv";
 dotenv.config();
 
+//serialization happens on Login (once on login!!)
 passport.serializeUser(function (user, done) {
-  done(null, user);
+  //for performance & security
+  //store only userId
+  const localUser = user as prismaUser;
+  done(null, localUser.id);
 });
 
-passport.deserializeUser(function (user: any, done) {
-  done(null, user);
+//on every request browser sends session cookie to server
+//deserializeUser take the id and fetches full user here
+passport.deserializeUser(async function (id: string, done) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
 });
 passport.use(
   new GitHubStrategy(
@@ -19,12 +36,22 @@ passport.use(
       callbackURL: process.env.GITHUB_CALLBACK_URL as string,
     },
     // Adding official type bindings to the verify callback function arguments
-    function (
+    async function (
       accessToken: string,
       refreshToken: string,
       profile: Profile,
       done: (error: any, user?: any) => void,
     ) {
+      //save user in database!!! (on login) (upsert = update or create)
+      const user = await prisma.user.upsert({
+        where: { github_id: profile.id },
+        update: { display_name: profile.displayName },
+        create: {
+          github_id: profile.id,
+          display_name: profile.displayName,
+          email: profile.emails?.[0]?.value ?? null,
+        },
+      });
       // Passes the profile metadata cleanly along to your callback router
       return done(null, profile);
     },

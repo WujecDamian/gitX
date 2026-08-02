79e7300c5075c3c25c8f31f0bc7579283e0cebb4
Damian Wujec <wujecdamian12@gmail.com>
2026-08-02 21:01:57 +0200

    refactor: modify login path to be more verbose

--- Backend/src/Routers/AuthRouter.ts
+++ Backend/src/Routers/AuthRouter.ts
@@ -7,7 +7,7 @@ const router = Router();
 // Routes
 router.get("/user", authController.getUser);
 router.get("/error", authController.loginError);
-router.get("/github", authController.authenticateUser);
+router.get("/login/github", authController.authenticateUser);
 router.get(
   "/github/callback",
   authController.callbackAuthenticate,

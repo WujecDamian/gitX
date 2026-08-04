e6db38273c039a1450454037ff6acfa7c6b62099
Damian Wujec <wujecdamian12@gmail.com>
2026-08-04 20:22:20 +0200

    fix: remove debug log from useAuth hook

--- Frontend/src/Contexts/Auth/AuthContext.tsx
+++ Frontend/src/Contexts/Auth/AuthContext.tsx
@@ -56,7 +56,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  console.log(context);
   if (!context) throw new Error("useAuth must be used within an AuthProvider");
   return context;
 };

43a98f36c250f1f0c1f4209224dd26e018b62e9a
Damian Wujec <wujecdamian12@gmail.com>
2026-08-04 15:01:05 +0200

    feat: implement login page layout and styling with logo integration

--- Frontend/src/Pages/Authentication/Login/Login.tsx
+++ Frontend/src/Pages/Authentication/Login/Login.tsx
@@ -1,12 +1,27 @@
+import styles from "./Login.module.css";
+
 const Login = () => {
   const handleLogin = () => {
     window.location.href = "http://localhost:3000/api/auth/login/github";
   };
 
   return (
-    <>
-      <button onClick={handleLogin}>Log in with GitHub</button>
-    </>
+    <main className={styles.login__main}>
+      <section className={styles.main__left}>
+        <h1>Code Connects.</h1>
+        <div className={styles.buttons}>
+          <button onClick={handleLogin}>Continue with GitHub</button>
+          <button onClick={handleLogin}>Continue as Guest</button>
+        </div>
+        <p>
+          By continuing, you agree to our Terms of Service, Privacy Policy and
+          Cookie Use.
+        </p>
+      </section>
+      <section className={styles.main__right}>
+        <img src="Frontend/public/X_logo_png.png" alt="GitX logo" />
+      </section>
+    </main>
   );
 };
 

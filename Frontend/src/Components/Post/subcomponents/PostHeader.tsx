7e2380e5a4a928c0faa30bce267a0a26e19e8c5e
Damian Wujec <wujecdamian12@gmail.com>
2026-08-07 23:43:48 +0200

    feat: wrap PostHeader content in Link for profile navigation

--- Frontend/src/Components/Post/subcomponents/PostHeader.tsx
+++ Frontend/src/Components/Post/subcomponents/PostHeader.tsx
@@ -1,6 +1,7 @@
 import { ProfilePicture } from "../../UI/ProfilePicture/ProfilePicture";
 import styles from "../PostCard.module.css";
 import { TimePosted } from "../../UI/Time/TimePosted";
+import { Link } from "react-router-dom";
 
 type PostHeaderTypes = {
   author: User;
@@ -10,7 +11,7 @@ type PostHeaderTypes = {
 export default function PostHeader(props: PostHeaderTypes) {
   return (
     <>
-      <div className={styles.post__header}>
+      <Link to={`/profile/${props.author.id}`} className={styles.post__header}>
         <ProfilePicture url={props.author.profile_picture_url}></ProfilePicture>
         <div className={styles.post__header__text__wrapper}>
           <h3 className="user__name">{props.author.display_name}</h3>
@@ -22,7 +23,7 @@ export default function PostHeader(props: PostHeaderTypes) {
 
           <TimePosted createTime={props.post.createdAt}></TimePosted>
         </div>
-      </div>
+      </Link>
     </>
   );
 }

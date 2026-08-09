ba0c02dc60c534d73cdb96c9f4fe1fdac16acee8
Damian Wujec <wujecdamian12@gmail.com>
2026-08-09 03:28:38 +0200

    feat: refactor follow button logic in ProfileCard component

--- Frontend/src/Components/ProfileCard/ProfileCard.tsx
+++ Frontend/src/Components/ProfileCard/ProfileCard.tsx
@@ -83,32 +83,6 @@ export default function ProfileCard({ owner, isFollowing }: props) {
       <div className={styles.action__row}>
         {owner.id === user?.id ? (
           <button className={styles.edit__button}>Edit profile</button>
-        ) : isFollowing ? (
-          <>
-            <button className={styles.message__button} onClick={onMessageClick}>
-              <svg
-                xmlns="http://w3.org"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
-              </svg>
-            </button>
-            <button
-              className={styles.following__button}
-              onClick={onFollowClick}
-              onMouseOver={() => setButtonContent("Unfollow")}
-              onMouseLeave={() => setButtonContent("Following")}
-            >
-              {buttonContent}
-            </button>
-          </>
         ) : (
           <>
             <button className={styles.message__button} onClick={onMessageClick}>
@@ -126,9 +100,21 @@ export default function ProfileCard({ owner, isFollowing }: props) {
                 <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
               </svg>
             </button>
-            <button className={styles.follow__button} onClick={onFollowClick}>
-              Follow
-            </button>
+
+            {isFollowing ? (
+              <button
+                className={styles.following__button}
+                onClick={onFollowClick}
+                onMouseOver={() => setButtonContent("Unfollow")}
+                onMouseLeave={() => setButtonContent("Following")}
+              >
+                {buttonContent}
+              </button>
+            ) : (
+              <button className={styles.follow__button} onClick={onFollowClick}>
+                Follow
+              </button>
+            )}
           </>
         )}
       </div>

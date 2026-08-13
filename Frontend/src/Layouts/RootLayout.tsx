fd581ee957609801cd21892c003b7d13ebc4ff62
Damian Wujec <wujecdamian12@gmail.com>
2026-08-13 17:04:58 +0200

    refactor: remove unused Navbar import and auth logic from RootLayout

--- Frontend/src/Layouts/RootLayout.tsx
+++ Frontend/src/Layouts/RootLayout.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Components/Navbar/Navbar";
 
 import { useAuth } from "../Contexts/Auth/AuthContext";
 
 function RootLayout() {
-  const { user, logout } = useAuth();
-
   return (
     <>
       <Outlet></Outlet>

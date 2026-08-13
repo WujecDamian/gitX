46b9d710351e7bc84d6a1b87541c9101e9d57491
Damian Wujec <wujecdamian12@gmail.com>
2026-08-13 23:52:31 +0200

    feat: add chat route for recipientId in routing configuration

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -36,6 +36,10 @@ const router = createBrowserRouter([
                 path: "/chat/",
                 element: <Chat></Chat>,
               },
+              {
+                path: "/chat/:recipientId",
+                element: <Chat></Chat>,
+              },
             ],
           },
         ],

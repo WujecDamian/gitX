dbe6fcee5b71fdf4ffa09da3236eb03373cf6bfd
Damian Wujec <wujecdamian12@gmail.com>
2026-08-15 13:52:35 +0200

    feat: define Chat and Message types for chat functionality

--- Frontend/src/Types/global.d.ts
+++ Frontend/src/Types/global.d.ts
@@ -63,6 +63,40 @@ declare global {
       sub_comments: number;
     };
   };
+  type Chat = {
+    id: string;
+    message: Message;
+    user1_id: string;
+    user2_id: string;
+  };
+  type Chat = {
+    id: string;
+    message: Message;
+    group_id: string;
+  };
+  type Message = {
+    chat_id: string;
+    content: string;
+    createdAt: string;
+    group_chat_id: string;
+    id: string;
+    media_url: string;
+    senderId: string;
+  };
 }
 
 export {};
+
+/*
+id
+: 
+"15455e29-9d4a-46eb-9f4a-ed37e3fe4830"
+messages
+: 
+[]
+user1_id
+: 
+"6edc16b9-628f-4d55-8ef9-e988e24d9e32"
+user2_id
+: 
+"7ceb5b76-2dda-47c6-ae29-854f20cd0d0f" */

5dbfad95636fd94031ee42923c8900d6acbc286c
Damian Wujec <wujecdamian12@gmail.com>
2026-08-17 23:04:42 +0200

    fix: correct import path for GroupListItem.module.css and update group link path

--- Frontend/src/Components/Group/Subcomponents/GroupListItem.tsx
+++ Frontend/src/Components/Group/Subcomponents/GroupListItem.tsx
@@ -1,4 +1,4 @@
-import styles from "./groupListItem.module.css";
+import styles from "./GroupListItem.module.css";
 import { ProfilePicture } from "../../UI/ProfilePicture/ProfilePicture";
 import { TimeSent } from "../../UI/Time/TimeSent";
 import { Link } from "react-router-dom";
@@ -9,7 +9,7 @@ type GroupListItemTypes = {
 
 export const GroupListItem = ({ group }: GroupListItemTypes) => {
   return (
-    <Link to={`/group/${group.id}`} className={styles.group__list__item}>
+    <Link to={`/groups/${group.id}`} className={styles.group__list__item}>
       <div className={styles["list__item--left"]}>
         <ProfilePicture url={group.group_profile_picture_url} />
       </div>
@@ -19,7 +19,12 @@ export const GroupListItem = ({ group }: GroupListItemTypes) => {
           <h4 className={styles.username}>{group.group_name}</h4>
         </div>
 
-        <div className={styles["item__right--bottom"]}></div>
+        <div className={styles["item__right--bottom"]}>
+          <p className={styles.members__preview}>
+            {group._count.members}{" "}
+            <span>{group._count.members > 1 ? `Members` : `Member`}</span>
+          </p>
+        </div>
       </div>
     </Link>
   );
